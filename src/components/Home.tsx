de01d684e9f4d0272bf43a3c0f03af817b356d1d
919849902@qq.com <919849902@qq.com>
2017-02-17 14:37:15 +0800

    require error

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
-
 class Tabbar extends React.Component<{}, {}> {
     render() {
         return (
             <div>
                 <div>
-                    <img src="./../assets/images/01.jpg" />
+                    <img src="http://tse4.mm.bing.net/th?id=OIP.-nN93biAZzjizAttKh4zkgEsDM&w=284&h=192&c=7&qlt=90&o=4&pid=1.7" />
                 </div>
             </div>
         );
--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Home from './components/Home';
 import './assets/content/weui.min.css';
-import './assets/less/base.less';
-import './assets/less/index.less';
+require('./assets/less/base.less');
 import Tabbar from './components/Tabbar';
 
 ReactDOM.render(
